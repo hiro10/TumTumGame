43f5e86d411016a5e49f559adc618f271d765f35
hiro10 <55058008+hiro10@users.noreply.github.com>
2022-09-28 23:32:02 +0900

    FirstCommit

---
+++ Assets/Sprites/BallGenerater.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallGenerater : MonoBehaviour
+{
+    // ball生成
+    // ballのプレハブの用意
+    // instantiateする
+    [SerializeField] GameObject ballPrehab = default;
+
+    // 画像の設定
+    // 設定する画像の用意
+    [SerializeField] Sprite[] ballSprites = default;
+
+    /// <summary>
+    /// ツムの生成
+    /// </summary>
+    public IEnumerator Spown(int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            // 生成位置をランダムに
+            Vector2 pos = new Vector2(Random.Range(-0.3f,0.3f), 8f);
+            // 生成 Quaternion.identity= 回転の初期値
+            GameObject ball = Instantiate(ballPrehab,pos,Quaternion.identity);
+
+            // 画像の設定
+            // ボールの種類だけランダムに
+            int ballID = Random.Range(0, ballSprites.Length);
+            ball.GetComponent<SpriteRenderer>().sprite = ballSprites[ballID];
+            ball.GetComponent<Ball>().id = ballID;
+            yield return new WaitForSeconds(0.04f);
+        }
+    }
+
+
+}
---
+++ Assets/Sprites/GameSystem.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// ゲームを管理
+public class GameSystem : MonoBehaviour
+{
+    // ツム生成用
+    [SerializeField] BallGenerater ballGenerater = default;
+
+    // ドラッグ中かの判定
+    bool isDragging;
+
+    //
+    [SerializeField] List<Ball> removeBalls = new List<Ball>();
+
+    /// <summary>
+    /// 開始処理
+    /// </summary>
+    private void Start()
+    {
+        StartCoroutine(ballGenerater.Spown(200));
+    }
+
+    /// <summary>
+    /// 更新処理
+    /// </summary>
+    private void Update()
+    {
+        // 右クリックを押し込んだ時
+        if (Input.GetMouseButtonDown(0))
+        {
+            OnDragin();
+        }
+        // 右クリックを離したとき
+        else if (Input.GetMouseButtonUp(0))
+        {
+            OnDragEnd();
+        }
+        else if(isDragging)
+        {
+            OnDriging();
+        }
+    }
+
+    /// <summary>
+    /// ドラッグをしたとき
+    /// </summary>
+    void OnDragin()
+    {
+        // オブジェクトのヒット確認
+        // Rayで判定
+        // カメラからスクリーンに向かって飛ばす
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(mousePosition,Vector2.zero); 
+
+        // ボールにヒットしたか
+        if(hit&& hit.collider.GetComponent<Ball>())
+        {
+            Debug.Log("オブジェクトにHitした");
+            Ball ball = hit.collider.GetComponent<Ball>();
+            AddRemoveBall(ball);
+
+            isDragging = true;
+        }
+    }
+    /// <summary>
+    /// ドラッグ中
+    /// </summary>
+    void OnDriging()
+    {
+        // オブジェクトのヒット確認
+        // Rayで判定
+        // カメラからスクリーンに向かって飛ばす
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
+
+        // ボールにヒットしたか
+        if (hit && hit.collider.GetComponent<Ball>())
+        {
+            Debug.Log("オブジェクトにHitした");
+            Ball ball = hit.collider.GetComponent<Ball>();
+            
+            AddRemoveBall(ball);
+        }
+    }
+    /// <summary>
+    /// ドラッグ終了時
+    /// </summary>
+    private void OnDragEnd()
+    {
+        int removeCount = removeBalls.Count;
+        for(int i = 0; i <removeCount;i++)
+        {
+            Destroy(removeBalls[i].gameObject);
+        }
+        removeBalls.Clear();
+        isDragging = false;
+    }
+
+    /// <summary>
+    /// リストがボールを持っていないかの確認
+    /// </summary>
+    /// <param name="ball"></param>
+    void AddRemoveBall(Ball ball)
+    {
+        // リストがBallを持っていなかったら
+        if(removeBalls.Contains(ball)==false)
+        {
+            // ballをリストに追加する
+            removeBalls.Add(ball);
+        }
+        
+    }
+}
