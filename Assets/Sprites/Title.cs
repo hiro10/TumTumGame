f7288a1688610a265b8850e99931c17cbacca39d
hiro10 <55058008+hiro10@users.noreply.github.com>
2022-10-02 17:26:08 +0900

    2022/10/02 ・タイトルの仮実装（シーンを切り替える形）

---
+++ Assets/Sprites/Title.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour
+{
+
+    public void OnStartButton()
+    {
+        // Mainシーンに遷移
+        SceneManager.LoadScene("Main");
+    }
+}
