bf656e63958635d6400c9b0029cb15f571e26b15
hiro10 <55058008+hiro10@users.noreply.github.com>
2022-10-12 00:10:37 +0900

    2022/10/11 ・textmespro導入 ・アスペクト比修正途中 ・文字を動かす処理は考え中

---
+++ Assets/Test.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using TMPro;
+
+public class Test : MonoBehaviour
+{
+    TextMeshProUGUI tmpro;
+
+    DOTweenTMPAnimator tmproAnimator; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        tmpro = GetComponent<TextMeshProUGUI>();
+        tmproAnimator = new DOTweenTMPAnimator(tmpro);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < tmproAnimator.textInfo.characterCount; ++i)
+        {
+            tmproAnimator.DOShakeCharOffset(i, 3f, 50f, 3, fadeOut: false).SetLoops(-1);
+        }
+    }
+}
